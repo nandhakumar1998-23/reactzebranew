c5f65947d7b4d104dc0abfdf1cf0db3a6595017d
B.Nandhakumar <nandhakumarid023@gmail.com>
2025-06-06 17:25:11 +0530

    Six.jsx

--- src/Components/Six.jsx
+++ src/Components/Six.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../assets/css/Six.css";
-import Volvo from '../assets/images/Volvo.png'
-import TVS from '../assets/images/TVS1.png'
-import CNC from '../assets/images/CMC.png'
-import FB from '../assets/images/FB1.png'
-import TG from '../assets/images/T__G1.png'
+import Volvo from '../assets/images/Volvo.png';
+import TVS from '../assets/images/TVS1.png';
+import CNC from '../assets/images/CMC.png';
+import FB from '../assets/images/FB1.png';
+import TG from '../assets/images/T__G1.png';
 
 const Six = () => {
   return (
